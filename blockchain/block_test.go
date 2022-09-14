74f661128222ac2db2362ef90d55ef405e664c2c
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-09-14 23:01:45 +0700

    Create the unit testing for calculate hash method

--- blockchain/block_test.go
+++ blockchain/block_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/erikrios/go-blockchain/utils"
 )
 
+const hashLen = 64
+
 func TestNewBlock(t *testing.T) {
 	t.Run("it should return non nil object, when the function is invoked", func(t *testing.T) {
 		senderName := utils.RandStr(5) + utils.RandStr(3)
@@ -17,6 +19,22 @@ func TestNewBlock(t *testing.T) {
 		block := NewBlock(utils.RandStr(64), *data, time.Now())
 		if block == nil {
 			t.Fatal("data shouldn't nil")
+		}
+	})
+}
+
+func TestCalculateHash(t *testing.T) {
+	t.Run("it should return 64 byte length, when CalculateHash method is invoked", func(t *testing.T) {
+		senderName := utils.RandStr(5) + utils.RandStr(3)
+		receiverName := utils.RandStr(10) + utils.RandStr(4)
+		amount := 200
+
+		data := NewData(senderName, receiverName, uint64(amount))
+		block := NewBlock(utils.RandStr(64), *data, time.Now())
+		got := block.CalculateHash()
+
+		if len(got) != hashLen {
+			t.Fatalf("got %d, want %d", len(got), hashLen)
 		}
 	})
 }
