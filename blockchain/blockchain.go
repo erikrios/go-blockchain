f8d54e659d33b3ce14b764216b56b4e278c49a38
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-09-14 23:36:23 +0700

    Create Blockchain entity

---
+++ blockchain/blockchain.go
@@ -0,0 +1,21 @@
+package blockchain
+
+import "time"
+
+type Blockchain struct {
+	Chain []Block
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{
+		Chain: []Block{
+			createGenesisBlock(),
+		},
+	}
+}
+
+func createGenesisBlock() Block {
+	data := NewData("", "", 0)
+	block := NewBlock("", *data, time.Now())
+	return *block
+}
