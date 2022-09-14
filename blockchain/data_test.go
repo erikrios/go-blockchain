84f4dae4d9a1c7160e15a5a035af48fd225e6707
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-09-14 22:14:56 +0700

    Refactor the test code

--- blockchain/data_test.go
+++ blockchain/data_test.go
@@ -1,10 +1,20 @@
 package blockchain
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/erikrios/go-blockchain/utils"
+)
 
 func TestNewData(t *testing.T) {
-	data := NewData("Bob", "Alice", 200)
-	if data == nil {
-		t.Fatal("data shouldn't nil")
-	}
+	t.Run("it should return non nil object, when the function is invoked", func(t *testing.T) {
+		senderName := utils.RandStr(5) + utils.RandStr(3)
+		receiverName := utils.RandStr(10) + utils.RandStr(4)
+		amount := 200
+
+		data := NewData(senderName, receiverName, uint64(amount))
+		if data == nil {
+			t.Fatal("data shouldn't nil")
+		}
+	})
 }
